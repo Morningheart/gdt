8e9f1c6695be7efd5b2719169f8156aaafccdbb7
TheVatsu <66404934+TheVatsu@users.noreply.github.com>
2021-12-02 20:41:35 +0100

    setup

--- resources/views/greeting.blade.php
+++ resources/views/greeting.blade.php
@@ -1,5 +1,26 @@
-<html>
-    <body>
-        <h1>Hello, {{ $name }}</h1>
-    </body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nuit de l'info 2021</title>
+    <link rel="stylesheet" href="https://unpkg.com/@vtmn/css" />
+</head>
+<body>
+    <script src="main.js"></script>
+    <div id="content">
+        <div id="menu">
+            <button>Inscription</button>
+            <button>Connexion</button>
+            <div class="vtmn-toggle">
+            <div class="vtmn-toggle_switch">
+                <input type="checkbox" id="theme" onchange="switch_theme()"/>
+                <span aria-hidden="true"></span>
+            </div>
+            <label for="theme">Thème sombre</label>
+        </div>
+        </div>
+    </div>
+</body>
 </html>
