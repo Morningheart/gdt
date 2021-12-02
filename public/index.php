cc370892928d35d8bd2c6b95127d8c0792880b4f
TheVatsu <66404934+TheVatsu@users.noreply.github.com>
2021-12-02 20:27:28 +0100

    bbbb

--- public/index.php
+++ public/index.php
@@ -13,20 +13,3 @@
 */
 
 $app->run(); ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Nuit de l'Info 2021</title>
-    <link rel="stylesheet" href="https://unpkg.com/@vtmn/css" />
-    <link rel="stylesheet" href="style.css">
-</head>
-<body>
-    <div id="content">
-        <?php include "html/menu.html" ?>
-    </div>
-    <script src="main.js"></script>
-</body>
-</html>
