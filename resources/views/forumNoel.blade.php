227e153cf2dce474509e930f302e781e624c0dbe
Morningheart <46131687+Morningheart@users.noreply.github.com>
2021-12-03 00:02:08 +0100

    Card noel affichage V2

--- resources/views/forumNoel.blade.php
+++ resources/views/forumNoel.blade.php
@@ -1,20 +1,17 @@
 <a href="/">
     <div class="block vtmn-flex vtmn-justify-center">
-    <div class="vtmn-card vtmn-card_variant--full-image" style="margin : 20 px; width : 80% ">
-        <div class="vtmn-card_image">
-        <img
-            src="https://storage.googleapis.com/dkt-design-cdn/images/placeholder.jpg"
+    <div class="vtmn-card vtmn-card_variant--full-image" style="width : 80%">
+        <div class="vtmn-card_image red-background">
+        <!-- <img
+            src="https://maisons-alfort.fr/wp-content/uploads/2019/01/sapin_noel.jpg"
             alt=""
-        />
+        /> -->
         </div>
         <div class="vtmn-card_content vtmn-card_content--opaque">
         <span class="vtmn-card_content--title">Joyeux Noël</span>
         <span class="vtmn-card_content--body">
             Souhaitez un Joyeux Noël à nos sauveur Quinkerkois !
         </span>
-        <button class="vtmn-btn vtmn-btn_variant--primary-reversed">
-            Olà !
-        </button>
         </div>
     </div>
     </div>
